fb83c86bd01e88a7760ac13f8ee7ca93545a5003
xDGxx <cntt.dg@gmail.com>
2024-04-28 10:47:41 -0300

    Sistema de funcionamento de uma Smart Tv com interação do usuário utilizandoo métodos

--- src/Usuario.java
+++ src/Usuario.java
@@ -1,2 +1,11 @@
 public class Usuario {
+    public static void main(String[] args) {
+
+        SmartTv smartTv = new SmartTv(); //Requisitando a classe "SmartTv" para "Usuario" criando um objeto com a mesma
+
+        System.out.println("Tv ligada? " +smartTv.ligada);
+        System.out.println("Canall atual? " +smartTv.canal);
+        System.out.println("Volume atual? " +smartTv.volume);
+
+    }
 }
