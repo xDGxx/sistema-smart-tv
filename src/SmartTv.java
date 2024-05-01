edcf7263ccab7a7c14f91da4da0ef6ed7df92569
xDGxx <cntt.dg@gmail.com>
2024-05-01 01:04:03 -0300

    Sistema de funcionamento de uma Smart Tv com interação do usuário utilizandoo métodos

--- src/SmartTv.java
+++ src/SmartTv.java
@@ -2,4 +2,39 @@ public class SmartTv {
     boolean ligada = false;
     int canal = 1;
     int volume = 25;
+
+    public void mudarCanal (int novoCanal){
+        canal = novoCanal;
+    }
+
+    public void ligar(){
+        ligada = true;
+    }
+
+    public void desligar() {
+        ligada = false;
+    }
+
+    public void avancarCanal(){
+        canal++;
+        System.out.println("Trocando o canal para " + canal);
+    }
+
+    public void voltarCanal(){
+        canal--;
+        System.out.println("Trocando o canal para " + canal);
+    }
+
+    public void aumentarVolume(){
+        volume++;
+        System.out.println("Aumentando o volume para " + volume);
+    }
+
+    public void diminuirVolume(){
+        volume--;
+        System.out.println("Diminuindo o volume para " + volume);
+    }
+
+
 }
+
